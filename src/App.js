054f3e067e7891aa23532bd9e6b0376afb0ce347
Yevhenii <y.lisyak@astoundcommerce.com>
2023-07-25 13:02:12 +0300

    Add: Access token

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+
+// UseEffect - run our code once at the beginning of the application 
+// useEffect(() => { }, []);
+
 
 // Forward the user to github login screen (we pass in the client ID)
 // User in now on the github side and logs in (github/login)
@@ -11,10 +16,30 @@ import './App.css';
 
 function App () {
   const CLIENT_ID = process.env.REACT_APP_GIT_CLIENT_ID;
+  const CLIENT_SECRET = process.env.REACT_APP_GIT_CLIENT_SECRET;
+  const [data, setData] = useState(null);
+  const [code, setCode] = useState(null);
+
+  useEffect(() => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    setCode(urlParams.get('code'));
+    console.log(code);
+  }, []);
+
+  const access_token_request = () => {
+    const params = '?client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET + '&code=' + code;
+
+    fetch('https://github.com/login/oauth/access_token' + params, { method: 'POST', headers: { "Accept": "application/json" } }).then(response => {
+      return response.json();
+    }).then(data => {
+      console.log(data);
+    });
+  };
 
   const loginWithGithub = () => {
     window.location.assign('https://github.com/login/oauth/authorize?client_id=' + CLIENT_ID);
-  }
+  };
   return (
     <div className="App">
       <header className="App-header">
@@ -22,6 +47,9 @@ function App () {
         <button onClick={loginWithGithub}>
           Login With Github
         </button>
+        {code && <button onClick={() => access_token_request()}>
+
+        </button>}
         <p>
           Lysiak Yevhenii
         </p>
