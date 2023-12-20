11e4110b003dc6a83e48a99a7d918089e3840924
Yevhenii <y.lisyak@astoundcommerce.com>
2023-12-20 19:57:54 +0200

    Add: media

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,23 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Menu.module.css';
 import salesforce_logo from '../../salesforce.svg';
 import react_logo from '../../react.svg';
 
 const Menu = () => {
+
+  console.log(window.matchMedia('(max-width: 767px)'));
+
+  const [width, setWidth] = useState(window.innerWidth);
+
+  function handleWindowSizeChange () {
+    setWidth(window.innerWidth);
+  }
+  useEffect(() => {
+    window.addEventListener('resize', handleWindowSizeChange);
+    return () => {
+      window.removeEventListener('resize', handleWindowSizeChange);
+    }
+  }, []);
+
+  const isMobile = width <= 768;
+
   return (
     <div className={styles.container}>
-      <div id={styles.panel}>
+      <div className={styles.panel}>
         <img className={styles['Salesforce-logo']} style={{ wight: "50px", height: "50px" }} src={salesforce_logo} alt="Salesforce Log" />
-        <div id={styles.hidden_panel}>
-          <div class={styles.display}>
+        <div className={styles.hidden_panel}>
+          <div className={styles.display}>
             {/* <iframe id="ytplayer" type="text/html" width="720" height="405" src="https://www.youtube.com/embed/qTv8lRXM6Tw" frameborder="0" allowfullscreen="" /> */}
           </div>
         </div>
       </div>
-      <div id={styles.panel} style={{ top: "88px" }}>
+      <div className={styles.panel} style={!isMobile ? { top: "88px" } : { left: "16%" }}>
         <img className={styles['App-logo']} style={{ wight: "50px", height: "42px" }} src={react_logo} alt="React Logo" />
-        <div id={styles.hidden_panel}>
-          <div class={styles.display}>
+        <div className={styles.hidden_panel}>
+          <div className={styles.display}>
             {/* <iframe id="ytplayer" type="text/html" width="720" height="405" src="https://www.youtube.com/embed/qTv8lRXM6Tw" frameborder="0" allowfullscreen="" /> */}
           </div>
         </div>
