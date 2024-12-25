fe2a7c0e053433972329ce9d8834a22b4f3b48f7
lysiak-yevhenii <lysiak.yevhenii@gmail.com>
2024-12-25 12:21:48 +0200

    Add: implement rotating cube with Three.js canvas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import react, { useEffect, useState } from 'react';
+import {Canvas, useFrame} from "@react-three/fiber";
+import { OrbitControls } from '@react-three/drei';
 import Menu from "./components/Menu/Menu";
 import MainPage from "./components/MainPage/MainPage";
 
 import "./index.css";
 import './App.css';
+import { OrbitControls } from '@react-three/drei';
+
+const RoatatingCube = () =>{
+  const meshRef = useRef();
+  useFrame(() => {
+    if (meshRef.current){
+      meshRef.current.roation.y += 0.01
+      meshRef.current.roation.x += 0.01
+    }
+  })
+  return (
+    <mesh ref={meshRef}>
+      <cylinderGeometry args={[1,1,1]}></cylinderGeometry>
+      <meshLambertMaterial color="#468585" emissive="#468585"></meshLambertMaterial>
+    </mesh>
+  )
+}
 
 function App () {
 
@@ -23,6 +42,12 @@ function App () {
 
   return (
     <div>
+      <Canvas style={{ height: '100vh', width: '100vw', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+        <OrbitControls enableZoom={true} enablePan></OrbitControls>
+        <directionalLight position={[1,1,1]} intensity={10} color={0x9CDBA6}></directionalLight>
+        <color attach="background" args={['#F0F0F0']}></color>
+        <RoatatingCube></RoatatingCube>
+      </Canvas>
       <Menu app-size={width} />
       <MainPage app-size={width} />
     </div>
