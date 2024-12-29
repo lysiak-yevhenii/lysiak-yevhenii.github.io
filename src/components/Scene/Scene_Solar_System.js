aedd1d7a740def3962ca8255a39ab3a103e8ca00
lysiak-yevhenii <lysiak.yevhenii@gmail.com>
2024-12-29 03:54:38 +0200

    Add: Shadow

--- src/components/Scene/Scene_Solar_System.js
+++ src/components/Scene/Scene_Solar_System.js
@@ -5,8 +5,32 @@ import PropTypes from 'prop-types';
 import * as THREE from 'three';
 
 const Sun = forwardRef((props, ref) => {
+    // Negative X direction light comes from left side
+    // Positive X direction light comes from right side
+    const directLightTarget = new THREE.Vector3(-1, 0, 0);
+    const directLightPostition = new THREE.Vector3(-10, 0, 0);
+
+
     return ( 
         <>  
+            {/* <LightDirection position={directLightPostition} target={directLightTarget} />
+            <mesh position={directLightPostition} castShadow>
+                <boxGeometry args={[1, 5, 5]} />
+                <directionalLight castShadow color={0xFFFFFF} intensity={5} position={directLightTarget} shadow-mapSize-width={1024}
+                    shadow-mapSize-height={1024}
+                    shadow-camera-near={0.5}
+                    shadow-camera-far={500}/>
+                <axesHelper args={[10]} />
+            </mesh> */}
+            <pointLight 
+                        position={[0, 0, 0]} 
+                        intensity={1000} 
+                        color="white" 
+                        castShadow 
+            /> 
+            
+            <ambientLight intensity={0.2} />
+
             {/* Red - X, Green - Y, Blue - Z */}
             <mesh ref={ref} position={[0, 0, 0]}> 
                 <axesHelper args={[10]} />
@@ -45,17 +69,25 @@ const Moon = ({ earthRef }) => {
     );
 };
 
-const Earth = forwardRef(({ sunRef }, ref) => {
-    const earthOrbitRef = useRef();
-    // Negative X direction light comes from left side
-    // Positive X direction light comes from right side
-    const directLightTarget = new THREE.Vector3(-1, 0, 0);
-    const directLightPostition = new THREE.Vector3(-10, 0, 0);
-    
+const LightDirection = ({ position, target }) => {
     const light = useRef();
     // Create a line to show the light path const 
-    const lightPath = new THREE.BufferGeometry().setFromPoints([ directLightPostition, directLightTarget ]);
+    const lightPath = new THREE.BufferGeometry().setFromPoints([ position, target ]);
+    
+    return (
+        <>
+            <line geometry={lightPath}> 
+                <lineBasicMaterial attach="material" color="yellow" /> 
+            </line> 
+            <pointLight ref={light} position={[0, 0, 0]} intensity={1} color="white" />
+        </>
+    );
+}
 
+const Earth = forwardRef(({ sunRef }, ref) => {
+    const earthOrbitRef = useRef();
+    
+    
     useFrame(({ clock }) => {
         const t = clock.getElapsedTime();
         earthOrbitRef.current.rotation.y = t * 0.2;
@@ -70,15 +102,7 @@ const Earth = forwardRef(({ sunRef }, ref) => {
                 <meshStandardMaterial color="blue" />
             </mesh>
             <Moon earthRef={ref} castShadow receiveShadow/>
-            <line geometry={lightPath}> 
-                <lineBasicMaterial attach="material" color="yellow" /> 
-            </line> 
-            <pointLight ref={light} position={[0, 0, 0]} intensity={1} color="white" />
-            <mesh position={directLightPostition} castShadow>
-                <boxGeometry args={[1, 5, 5]} />
-                <directionalLight castShadow color={0xFFFFFF} intensity={5} position={directLightTarget}/>
-                <axesHelper args={[10]} />
-            </mesh>
+            
         </group>
     );
 });
@@ -101,7 +125,6 @@ const SolarSystem = () => {
         <> 
             <Sun ref={sunRef} /> 
             <Earth sunRef={sunRef} ref={earthRef} /> 
-            <ambientLight intensity={0.1} />
         </> 
     );
 }
